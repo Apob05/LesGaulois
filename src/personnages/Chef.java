60174848932dc3120c0697353b18884d4c613be1
Apob05 <129179145+Apob05@users.noreply.github.com>
2024-02-19 16:08:00 +0100

    19-02
    village creation

--- src/personnages/Chef.java
+++ src/personnages/Chef.java
@@ -1,13 +1,14 @@
 package personnages;
 
-public class Gaulois {
+public class Chef {
 	private String nom;
 	private int force;
-	private int effetPotion = 1 ;
+	private Village village;
 
-	public Gaulois(String nom, int force) {
+	public Chef(String nom, int force, Village village) {
 		this.nom = nom;
 		this.force = force;
+		this.village = village;
 	}
 
 	public String getNom() {
@@ -20,22 +21,11 @@ public class Gaulois {
 	}
 
 	private String prendreParole() {
-		return "Le gaulois" + nom + ":";
+		return "le chef" + nom + "du village " + village.getNom() + ":";
 	}
 
 	public void frapper(Romain romain) {
 		System.out.println(nom + "envoie un grand coup dans la mâchoire de" + romain.getNom());
 		romain.recevoirCoup(force / 3);
 	}
-
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
-	
-	public static void main(String[] args) {
-		
-	}
-	
-
 }
